0d65fd476470b47417b516f55e54a730fd0905ae
buithetuan <132962473+buithetuan@users.noreply.github.com>
2024-11-07 22:56:59 +0700

    Delete postprocessing directory

--- postprocessing/grammar_check.py
+++
@@ -1,6 +0,0 @@
-# postprocessing/grammar_check.py
-
-def grammar_check(text):
-    # Giả lập kiểm tra ngữ pháp
-    corrected_text = text  # Placeholder
-    return corrected_text
--- postprocessing/spell_check.py
+++
@@ -1,6 +0,0 @@
-# postprocessing/spell_check.py
-
-def spell_check(text):
-    # Giả lập kiểm tra chính tả
-    corrected_text = text  # Placeholder
-    return corrected_text
