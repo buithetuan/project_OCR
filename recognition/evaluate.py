9d6a0230b642e7edb9ed41ed8a66ecf8ff8a967d
buithetuan <132962473+buithetuan@users.noreply.github.com>
2024-11-08 09:51:55 +0700

    Update evaluate.py

--- recognition/evaluate.py
+++ recognition/evaluate.py
@@ -2,12 +2,10 @@ import tensorflow as tf
 from utils.helper_functions import load_data
 
 def evaluate_model():
-    # Tải dữ liệu kiểm tra với giới hạn 10 ảnh mỗi ký tự cho test
     x_test, y_test = load_data('data_set/testing_data', 'labels/testing_labels.csv', max_images_per_class=10)
     
     model = tf.keras.models.load_model('ocr_model.h5')
-    
-    # Đánh giá mô hình trên dữ liệu kiểm tra
+
     loss, accuracy = model.evaluate(x_test, y_test)
     
     print(f"Loss: {loss}, Accuracy: {accuracy}")
